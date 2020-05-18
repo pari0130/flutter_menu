a64e7e3a7482d1ead52941f61dd85977001627e0
donghwi <pari0130@gmail.com>
2020-05-18 23:23:12 +0900

    20200518 study

--- lib/menu_dashboard_layout.dart
+++ lib/menu_dashboard_layout.dart
@@ -11,13 +11,56 @@ class MenuDashBoardPage extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           menu(context),
-          //dashboard(context),
+          dashboard(context),
         ],
       ),
     );
   }
 
-  Widget menu(context){
+  Widget menu(context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Dashboard",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Messages",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Utility Bills",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Funds Transfer",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Branches",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
+  Widget dashboard(context){
+    return Material(
+      elevation: 8,
+      color: ,
+    )
   }
 }
